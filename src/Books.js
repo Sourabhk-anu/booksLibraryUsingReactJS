53ddf803816589eb51b6e30fecd19a296acfe109
sourabh kumar <sk1272883@gmail.com>
2021-12-30 18:00:05 -0500

    new commited

--- src/Books.js
+++ src/Books.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const Books = ({book}) => {
-    const {title, authors, imageLinks}= book;
+    const {title, authors, imageLinks, publisher, publishedDate, description}= book;
     return (
-        <div>
+        <div className="book-detail">
             <h1>Books name: {title}</h1>
             <h2>Authors: {authors && authors.map(author=> <span>{author}</span>)}</h2>
+            <h2>Publisher: {publisher}</h2>
+            <h2>Publish Date: {publishedDate}</h2>
             <img src={imageLinks.thumbnail} alt={title}/>
+            <p><b>Description:</b> {description}</p>
         </div>
     )
 }
